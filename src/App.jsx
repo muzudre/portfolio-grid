f7bb3b4a78bee066bfec60dd587b89b89e426aee
Muslim Zabirov <muzudre@gmail.com>
2022-04-06 23:04:02 +0500

    first commit
    grid system in tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
 function App() {
-
   return (
-    <div className="App">
-<h1>hello</h1>
-    </div>
-  )
+    <main className="grid place-items-center min-h-screen">
+      <div className="p-4 max-w-5xl grid gap-4 sm:grid-cols-2 md:grid-cols-4">
+        <h1 className="text-4xl font-extrabold sm:col-span-2 sm:grid sm:gap-4 sm:grid-cols-2 md:col-span-3 md:text-5xl md:grid-cols-3 lg:text-6xl">
+          <span className="md:col-span-2">Grid layout with Tailwind CSS.</span>
+        </h1>
+        <p className="sm:row-start-2 sm:col-start-2 sm:self-center md:col-start-1 md:col-span-2 md:pr-12 md:text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sit
+          explicabo inventore repellendus.
+        </p>
+        <div className="h-16 bg-blue-500 sm:h-auto sm:square"></div>
+        <div className="h-16 bg-blue-500 sm:h-auto sm:square"></div>
+        <div className="h-16 bg-red-500 sm:h-auto sm:square"></div>
+        <div className="h-16 bg-blue-500 sm:h-auto sm:square md:col-start-2"></div>
+        <div className="h-16 bg-red-500 sm:h-auto sm:square"></div>
+        <div className="h-16 bg-blue-500 sm:h-auto sm:square"></div>
+        <div className="h-16 bg-blue-500 sm:h-auto sm:square"></div>
+        <div className="h-16 bg-red-500 sm:h-auto sm:square"></div>
+        <p className="self-center md:text-lg md:col-span-2 md:text-center md:px-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet sit
+          explicabo inventore repellendus.
+        </p>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
